b5808fff8b1d0b13e5e317312f8383961dbe50f7
JJAMES001@e.ntu.edu.sg <joshjms1607@gmail.com>
2023-06-27 16:53:29 +0800

    stuff

--- server.go
+++ server.go
@@ -21,7 +21,7 @@ func main() {
 	})
 
 	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
-		url := "https://test.gtp-trial.teleport.sh/query/"
+		url := "https://test.gtp-trial.teleport.sh/query"
 
 		// GraphQL query
 		query := `
@@ -33,32 +33,26 @@ func main() {
 			}
 		`
 
-		// Create an HTTP client
 		client := &http.Client{}
 
-		// Create a JSON payload with the query
 		payload := map[string]interface{}{
 			"query": query,
 		}
 
-		// Convert payload to JSON
 		payloadBytes, err := json.Marshal(payload)
 		if err != nil {
 			fmt.Println("Failed to marshal JSON payload:", err)
 			return
 		}
 
-		// Create an HTTP POST request with the payload
 		req, err := http.NewRequest("POST", url, bytes.NewBuffer(payloadBytes))
 		if err != nil {
 			fmt.Println("Failed to create request:", err)
 			return
 		}
 
-		// Set headers
 		req.Header.Set("Content-Type", "application/json")
 
-		// Send the request
 		resp, err := client.Do(req)
 		if err != nil {
 			fmt.Println("Failed to send request:", err)
@@ -66,7 +60,6 @@ func main() {
 		}
 		defer resp.Body.Close()
 
-		// Read the response body
 		body, err := ioutil.ReadAll(resp.Body)
 		if err != nil {
 			fmt.Println("Failed to read response body:", err)
